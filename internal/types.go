069803cb1ec376cbc8b26397f5aca0f0eebe069c
Wax, Bence <bence.wax@lhsystems.com>
2021-11-08 20:41:16 +0100

    "Initial commit"

---
+++ internal/types.go
@@ -0,0 +1,9 @@
+package internal
+import (
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+)
+type Type__cdktfTerraformResource = cdktf.TerraformResource
+type Type__cdktfComplexObject = cdktf.ComplexObject
+type Type__cdktfComplexComputedList = cdktf.ComplexComputedList
+type Type__cdktfTerraformProvider = cdktf.TerraformProvider
+type Type__cdktfTerraformDataSource = cdktf.TerraformDataSource
